8b97d002051ead34e634a9374e7691be38dbe6bd
looseyi <13615033587@126.com>
2016-05-07 23:58:57 +0800

    Initial Commit

---
+++ ProgressHUD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgressHUD
+//
+//  Created by Edmond on 5/7/2559 BE.
+//  Copyright © 2559 BE Edmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
